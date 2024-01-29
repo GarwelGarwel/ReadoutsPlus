f5daa1e7b55a8cf80f8ff01bff88a671956a16d2
Garwel <garwel@protonmail.com>
2024-01-29 12:36:15 +0100

    Add project files.

---
+++ Source/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadoutsPlus
+{
+    public class Class1
+    {
+    }
+}
