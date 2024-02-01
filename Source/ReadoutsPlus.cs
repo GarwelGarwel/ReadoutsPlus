9cfccc65a3c0a2a9a0b2ee94b35cdf6f634cee9a
Garwel <garwel@protonmail.com>
2024-02-01 22:49:56 +0100

    Fixed click-through and clicks not working on resource categories; optimized algorithm a bit

--- Source/ReadoutsPlus.cs
+++ Source/ReadoutsPlus.cs
@@ -13,27 +13,18 @@ namespace ReadoutsPlus
     {
         static ReadoutsPlus()
         {
-            Harmony harmony = new Harmony("ReadoutsPlus");
+            Harmony harmony = new Harmony("Garwel.ReadoutsPlus");
             if (harmony.Patch(AccessTools.Method("Listing_ResourceReadout:DoThingDef"), postfix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("Listing_ResourceReadout_DoThingDef"))) == null
                 || harmony.Patch(AccessTools.Method("ResourceReadout:DrawResourceSimple"), postfix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("ResourceReadout_DrawResourceSimple"))) == null
                 || harmony.Patch(AccessTools.Method("Listing_ResourceReadout:DoCategory"), prefix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("Listing_ResourceReadout_DoCategory"))) == null)
-                Log("Failed to apply Harmony patch!", true);
-        }
-
-        static void Log(string message, bool important = false)
-        {
-            if (important || Prefs.DevMode)
-                Verse.Log.Message($"[Readouts+] {message}");
+                Log.Error($"[Readouts+] Failed to initialize. Harmony patches applied: {harmony.GetPatchedMethods().Select(method => method.Name).ToCommaList()}.");
         }
 
         static void SelectNextThing(ThingDef thingDef)
         {
             List<Thing> things = Find.CurrentMap.haulDestinationManager.AllGroupsListForReading.SelectMany(group => group.HeldThings).Where(thing => thing.def == thingDef).ToList();
             if (things.NullOrEmpty())
-            {
-                Log($"List of {thingDef} on the map is null or empty.", true);
                 return;
-            }
             int i = 0;
             Thing selected = Find.Selector.SingleSelectedThing;
             if (selected?.def == thingDef)
@@ -54,9 +45,9 @@ namespace ReadoutsPlus
                 Find.Selector.Select(thing);
         }
 
-        static void ProcessClick(Rect rect, ThingDef thingDef)
+        static void CheckClick(ThingDef thingDef)
         {
-            if (Mouse.IsOver(rect) && Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0)
             {
                 if (Event.current.clickCount == 1)
                     SelectNextThing(thingDef);
@@ -68,25 +59,40 @@ namespace ReadoutsPlus
 
         public static void Listing_ResourceReadout_DoThingDef(Listing_ResourceReadout __instance, ThingDef thingDef, int nestLevel)
         {
-            if (Find.CurrentMap.resourceCounter.GetCount(thingDef) > 0)
-                ProcessClick(
-                    new Rect(nestLevel * __instance.nestIndentWidth + 18, __instance.CurHeight - __instance.lineHeight - __instance.verticalSpacing, __instance.ColumnWidth, __instance.lineHeight),
-                    thingDef);
+            if (Mouse.IsOver(new Rect(nestLevel * __instance.nestIndentWidth + 18, __instance.CurHeight - __instance.lineHeight - __instance.verticalSpacing, __instance.ColumnWidth, __instance.lineHeight))
+                && Find.CurrentMap.resourceCounter.GetCount(thingDef) > 0)
+                CheckClick(thingDef);
         }
 
         public static void ResourceReadout_DrawResourceSimple(Rect rect, ThingDef thingDef)
         {
-            if (Find.CurrentMap.resourceCounter.GetCount(thingDef) == 0)
-                return;
-            if (Mouse.IsOver(rect))
+            if (Mouse.IsOver(rect) && Find.CurrentMap.resourceCounter.GetCount(thingDef) > 0)
+            {
                 GUI.DrawTexture(rect, TexUI.HighlightTex);
-            ProcessClick(rect, thingDef);
+                CheckClick(thingDef);
+            }
+        }
+
+        static bool AnyOfCategory(ThingCategoryDef categoryDef)
+        {
+            Map map = Find.CurrentMap;
+            for (int i = 0; i < categoryDef.childThingDefs.Count; i++)
+                if (map.resourceCounter.GetCount(categoryDef.childThingDefs[i]) > 0)
+                    return true;
+            for (int i = 0; i < categoryDef.childCategories.Count; i++)
+                if (AnyOfCategory(categoryDef.childCategories[i]))
+                    return true;
+            return false;
         }
 
         public static void Listing_ResourceReadout_DoCategory(Listing_ResourceReadout __instance, TreeNode_ThingCategory node, int nestLevel, int openMask)
         {
             Rect rect = new Rect(nestLevel * __instance.nestIndentWidth + 18, __instance.CurHeight, __instance.ColumnWidth, __instance.lineHeight);
-            if (Mouse.IsOver(rect) && Event.current.type == EventType.MouseDown && Event.current.button == 0)
+            if (Mouse.IsOver(rect)
+                && Event.current.type == EventType.MouseDown
+                && Event.current.button == 0
+                && AnyOfCategory(node.catDef))
+            {
                 if (__instance.IsOpen(node, openMask))
                 {
                     SoundDefOf.TabClose.PlayOneShotOnCamera();
@@ -97,6 +103,8 @@ namespace ReadoutsPlus
                     SoundDefOf.TabOpen.PlayOneShotOnCamera();
                     node.SetOpen(openMask, true);
                 }
+                Event.current.Use();
+            }
         }
     }
 }
