9283b11557600f705c3b232818c33a3cf894ca4a
Garwel <garwel@protonmail.com>
2024-03-20 23:32:37 +0100

    Added saving and loading of open categories; fixed some categories not opening/closing on clicking

---
+++ Source/GameComponent_Readouts.cs
@@ -0,0 +1,78 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace ReadoutsPlus
+{
+    public class GameComponent_Readouts : GameComponent
+    {
+        static readonly List<ThingCategoryDef> emptyList = new List<ThingCategoryDef>();
+
+        List<ThingCategoryDef> readoutCategoriesCached;
+
+        public List<ThingCategoryDef> ReadoutCategories
+        {
+            get
+            {
+                if (readoutCategoriesCached != null)
+                    return readoutCategoriesCached;
+                if (Find.MapUI == null)
+                {
+                    ReadoutsPlus.Log("MapUI is null.", true);
+                    return emptyList;
+                }
+                ResourceReadout resourceReadout = AccessTools.Field(typeof(MapInterface), "resourceReadout").GetValue(Find.MapUI) as ResourceReadout;
+                if (resourceReadout != null)
+                    if ((readoutCategoriesCached = AccessTools.Field(typeof(ResourceReadout), "RootThingCategories").GetValue(resourceReadout) as List<ThingCategoryDef>) != null)
+                        return readoutCategoriesCached;
+                    else ReadoutsPlus.Log("Could not access RootThingCategories.", true);
+                else ReadoutsPlus.Log("Could not access resourceReadout.", true);
+                return emptyList;
+            }
+        }
+
+        public GameComponent_Readouts(Game game)
+            : base()
+        { }
+
+        public void SetOpenCategories(List<ThingCategoryDef> openCategories)
+        {
+            foreach (ThingCategoryDef cat1 in ReadoutCategories)
+                foreach (ThingCategoryDef cat2 in cat1.ThisAndChildCategoryDefs)
+                    cat2.treeNode.SetOpen(ReadoutsPlus.OpenMask, openCategories.Contains(cat2));
+        }
+
+        public void CloseAllCategories()
+        {
+            foreach (ThingCategoryDef cat1 in ReadoutCategories)
+                foreach (ThingCategoryDef cat2 in cat1.ThisAndChildCategoryDefs)
+                    cat2.treeNode.SetOpen(ReadoutsPlus.OpenMask, false);
+        }
+
+        public override void ExposeData()
+        {
+            ReadoutsPlus.Log($"GameComponent_Readouts.ExposeData (mode: {Scribe.mode})");
+            List<ThingCategoryDef> openCategories = null;
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                openCategories = ReadoutCategories.GetOpenCategories();
+                ReadoutsPlus.Log($"Current open categories:\n{ReadoutsPlus.CategoriesDescription(openCategories)}");
+                if (openCategories.NullOrEmpty())
+                    return;
+            }
+            Scribe_Collections.Look(ref openCategories, "OpenCategories", LookMode.Def);
+            if (Scribe.mode == LoadSaveMode.LoadingVars)
+            {
+                if (openCategories == null)
+                {
+                    ReadoutsPlus.Log("OpenCategories not found.", true);
+                    CloseAllCategories();
+                    return;
+                }
+                ReadoutsPlus.Log($"Loaded open categories:\n{ReadoutsPlus.CategoriesDescription(openCategories)}");
+                SetOpenCategories(openCategories);
+            }
+        }
+    }
+}
--- Source/ReadoutsPlus.cs
+++ Source/ReadoutsPlus.cs
@@ -11,38 +11,15 @@ namespace ReadoutsPlus
     [StaticConstructorOnStartup]
     public static class ReadoutsPlus
     {
+        internal const int OpenMask = 32;
+
         static ReadoutsPlus()
         {
             Harmony harmony = new Harmony("Garwel.ReadoutsPlus");
             if (harmony.Patch(AccessTools.Method("Listing_ResourceReadout:DoThingDef"), postfix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("Listing_ResourceReadout_DoThingDef"))) == null
                 || harmony.Patch(AccessTools.Method("ResourceReadout:DrawResourceSimple"), postfix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("ResourceReadout_DrawResourceSimple"))) == null
                 || harmony.Patch(AccessTools.Method("Listing_ResourceReadout:DoCategory"), prefix: new HarmonyMethod(typeof(ReadoutsPlus).GetMethod("Listing_ResourceReadout_DoCategory"))) == null)
-                Log.Error($"[Readouts+] Failed to initialize. Harmony patches applied: {harmony.GetPatchedMethods().Select(method => method.Name).ToCommaList()}.");
-        }
-
-        static void SelectNextThing(ThingDef thingDef)
-        {
-            List<Thing> things = Find.CurrentMap.haulDestinationManager.AllGroupsListForReading.SelectMany(group => group.HeldThings).Where(thing => thing.def == thingDef).ToList();
-            if (things.NullOrEmpty())
-                return;
-            int i = 0;
-            Thing selected = Find.Selector.SingleSelectedThing;
-            if (selected?.def == thingDef)
-            {
-                while (i < things.Count)
-                    if (things[i++] == selected)
-                        break;
-                if (i >= things.Count)
-                    i = 0;
-            }
-            CameraJumper.TryJumpAndSelect(things[i]);
-        }
-
-        static void SelectAll(ThingDef thingDef)
-        {
-            Find.Selector.ClearSelection();
-            foreach (Thing thing in Find.CurrentMap.haulDestinationManager.AllGroupsListForReading.SelectMany(group => group.HeldThings).Where(thing => thing.def == thingDef))
-                Find.Selector.Select(thing);
+                Log($"Failed to initialize. Harmony patches applied: {harmony.GetPatchedMethods().Select(method => method.Name).ToCommaList()}.", true);
         }
 
         static void CheckClick(ThingDef thingDef)
@@ -56,6 +33,8 @@ namespace ReadoutsPlus
                 Event.current.Use();
             }
         }
+
+        #region HARMONY PATCHES
 
         public static void Listing_ResourceReadout_DoThingDef(Listing_ResourceReadout __instance, ThingDef thingDef, int nestLevel)
         {
@@ -73,38 +52,98 @@ namespace ReadoutsPlus
             }
         }
 
-        static bool AnyOfCategory(ThingCategoryDef categoryDef)
-        {
-            Map map = Find.CurrentMap;
-            for (int i = 0; i < categoryDef.childThingDefs.Count; i++)
-                if (map.resourceCounter.GetCount(categoryDef.childThingDefs[i]) > 0)
-                    return true;
-            for (int i = 0; i < categoryDef.childCategories.Count; i++)
-                if (AnyOfCategory(categoryDef.childCategories[i]))
-                    return true;
-            return false;
-        }
-
         public static void Listing_ResourceReadout_DoCategory(Listing_ResourceReadout __instance, TreeNode_ThingCategory node, int nestLevel, int openMask)
         {
             Rect rect = new Rect(nestLevel * __instance.nestIndentWidth + 18, __instance.CurHeight, __instance.ColumnWidth, __instance.lineHeight);
             if (Mouse.IsOver(rect)
                 && Event.current.type == EventType.MouseDown
                 && Event.current.button == 0
-                && AnyOfCategory(node.catDef))
+                && AnyOfCategoryOnCurrentMap(node.catDef))
             {
-                if (__instance.IsOpen(node, openMask))
-                {
+                bool isOpen = __instance.IsOpen(node, openMask);
+                Log($"Click on {node.catDef.defName} category. It is currently {(isOpen ?  "open" : "closed")}.");
+                if (isOpen)
                     SoundDefOf.TabClose.PlayOneShotOnCamera();
-                    node.SetOpen(openMask, false);
-                }
-                else
-                {
-                    SoundDefOf.TabOpen.PlayOneShotOnCamera();
-                    node.SetOpen(openMask, true);
-                }
+                else SoundDefOf.TabOpen.PlayOneShotOnCamera();
+                node.SetOpen(openMask, !isOpen);
                 Event.current.Use();
             }
         }
+
+        #endregion
+
+        #region UTILITIES
+
+        internal static void Log(string message, bool important = false)
+        {
+            if (important)
+                Verse.Log.Warning($"[Readouts+] {message}");
+#if DEBUG
+            else Verse.Log.Message($"[Readouts+] {message}");
+#endif
+        }
+
+        public static void SelectNextThing(ThingDef thingDef)
+        {
+            HaulDestinationManager haulDestinationManager = Find.CurrentMap?.haulDestinationManager;
+            if (haulDestinationManager == null || thingDef == null)
+                return;
+            List<Thing> things = haulDestinationManager.AllGroupsListForReading.SelectMany(group => group.HeldThings).Where(thing => thing.def == thingDef).ToList();
+            if (things.NullOrEmpty())
+                return;
+            int i = 0;
+            Thing selected = Find.Selector.SingleSelectedThing;
+            if (selected?.def == thingDef)
+            {
+                while (i < things.Count)
+                    if (things[i++] == selected)
+                        break;
+                if (i >= things.Count)
+                    i = 0;
+            }
+            CameraJumper.TryJumpAndSelect(things[i]);
+        }
+
+        public static void SelectAll(ThingDef thingDef)
+        {
+            Find.Selector.ClearSelection();
+            HaulDestinationManager haulDestinationManager = Find.CurrentMap?.haulDestinationManager;
+            if (haulDestinationManager == null || thingDef == null)
+                return;
+            foreach (Thing thing in haulDestinationManager.AllGroupsListForReading.SelectMany(group => group.HeldThings).Where(thing => thing.def == thingDef))
+                Find.Selector.Select(thing);
+        }
+
+        public static bool AnyOfCategoryOnCurrentMap(ThingCategoryDef categoryDef)
+        {
+            Map map = Find.CurrentMap;
+            if (map == null || categoryDef == null)
+                return false;
+            for (int i = 0; i < categoryDef.childThingDefs.Count; i++)
+                if (map.resourceCounter.GetCount(categoryDef.childThingDefs[i]) > 0)
+                    return true;
+            for (int i = 0; i < categoryDef.childCategories.Count; i++)
+                if (!categoryDef.childCategories[i].resourceReadoutRoot && AnyOfCategoryOnCurrentMap(categoryDef.childCategories[i]))
+                    return true;
+            return false;
+        }
+
+        public static bool IsOpen(this ThingCategoryDef category) => category.treeNode.IsOpen(OpenMask);
+
+        public static List<ThingCategoryDef> GetOpenCategories(this IEnumerable<ThingCategoryDef> categories)
+        {
+            List<ThingCategoryDef> list = new List<ThingCategoryDef>();
+            foreach (ThingCategoryDef cat1 in categories)
+                foreach (ThingCategoryDef cat2 in cat1.ThisAndChildCategoryDefs)
+                    if (cat2.IsOpen() && !list.Contains(cat2))
+                        list.Add(cat2);
+            return list;
+        }
+
+        public static string ThingCategoryDescription(this ThingCategoryDef def) => $"{def.defName} ({(IsOpen(def) ? "open" : "closed")})";
+
+        public static string CategoriesDescription(List<ThingCategoryDef> categories) => categories?.Select(cat => ThingCategoryDescription(cat)).ToLineList("- ");
+
+#endregion
     }
 }
